df463fafc4d1cd91f9b73497345f059cf480a463
Mfoniso Fabian <goodlucketim94@gmail.com>
2023-01-03 22:19:20 +0200

    update

---
+++ src/components/about/HeroComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hero">
+    <div class="about-container">
+      <div class="aboutTitle">
+        <h3>About Us</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi vero
+          obcaecati quae qui alias odio rem aliquam facilis deleniti nobis.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "aboutHero",
+};
+</script>
+
+<style>
+.hero {
+  background: url("../../assets/background\ \(2\).jpg");
+  height: 70vh;
+}
+.about-container {
+  width: 80%;
+  margin: 0 auto;
+  padding: 10px;
+  color: #f5f5f5;
+}
+.aboutTitle {
+  position: absolute;
+  top: 40%;
+  font-size: x-large;
+  width: 50%;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="aboutcontainer">
+      <AboutHero />
+    </div>
   </div>
 </template>
+
+<script>
+import AboutHero from "@/components/about/HeroComponent.vue";
+export default {
+  name: "AboutView",
+  components: {
+    AboutHero,
+  },
+};
+</script>
