d9629f9885a87ccc59f4156e5c048d8c183a5889
Mfoniso Fabian <goodlucketim94@gmail.com>
2023-01-21 16:43:56 +0100

    update

--- src/components/home/serviceComponent.vue
+++ src/components/home/serviceComponent.vue
@@ -6,10 +6,12 @@
         <div class="service">
           <font-awesome-icon class="icon" icon="fa-solid fa-code" size="4x" />
           <h3>Web Development</h3>
+          <a class="action" href="#">Get Started</a>
         </div>
         <div class="service">
           <font-awesome-icon class="icon" icon="fa-solid fa-pencil" size="4x" />
           <h3>Graphic Design</h3>
+          <a class="action" href="#">Get Started</a>
         </div>
         <div class="service">
           <font-awesome-icon
@@ -18,6 +20,7 @@
             size="4x"
           />
           <h3>Data Analysis</h3>
+          <a class="action" href="#">Get Started</a>
         </div>
         <div class="service">
           <font-awesome-icon
@@ -26,6 +29,7 @@
             size="4x"
           />
           <h3>Project Management</h3>
+          <a class="action" href="#">Get Started</a>
         </div>
         <div class="service">
           <font-awesome-icon
@@ -34,6 +38,7 @@
             size="4x"
           />
           <h3>UI/UX Design</h3>
+          <a class="action" href="#">Get Started</a>
         </div>
         <div class="service">
           <font-awesome-icon
@@ -42,6 +47,7 @@
             size="4x"
           />
           <h3>Training</h3>
+          <a class="action" href="#">Get Started</a>
         </div>
       </div>
     </div>
@@ -67,24 +73,42 @@ export default {
 .service-items {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-gap: 2rem;
+  grid-gap: 3rem;
 }
 .service {
   text-align: center;
-  height: 20vh;
-  box-shadow: 0 5px 10px 5px rgb(210, 196, 196);
+  box-shadow: 0 0px 15px 1px rgb(210, 196, 196);
   padding: 1rem;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 }
+.service:hover {
+  transform: scale(1.05);
+  transition: all 0.5s;
+}
 .service h3 {
   color: #01091d;
-  font-size: x-large;
+  font-size: larger;
+  margin: 10px;
 }
 .icon {
   color: #03001e;
+}
+.action {
+  padding: 15px;
+  margin: 15px auto;
+  border-radius: 10px;
+  background: #03001e;
+  color: #fff;
+  text-decoration: none;
+}
+.action:hover {
+  color: #03001e;
+  background: #fff;
+  border: 1px inset #03001e;
+  transition: all 0.5s;
 }
 @media screen and (max-width: 500px) {
   .service-items {
---
+++ src/views/TrainingView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <div class="aboutcontainer">
+      <AboutHero />
+      <serviceComponent />
+      <FooterComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+import AboutHero from "@/components/about/HeroComponent.vue";
+import serviceComponent from "@/components/home/serviceComponent.vue";
+import FooterComponent from "@/components/home/FooterComponent.vue";
+export default {
+  name: "TrainingView",
+  components: {
+    AboutHero,
+    serviceComponent,
+    FooterComponent,
+  },
+};
+</script>
