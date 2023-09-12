2ec7c4ded8f13c0233d61dbfe04bc421b4877057
Mfoniso Fabian <goodlucketim94@gmail.com>
2023-09-12 12:44:48 +0100

    update

--- src/components/TemporaryComponent.vue
+++ src/components/TemporaryComponent.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <div class="temporary-display flex flex-col w-10/12 justify-center align-center gap-5 mx-auto text-center p-16">
+    <div class="temporary-display flex flex-col w-10/12 justify-center align-center gap-5 mx-auto text-center p-16 sm:w-11/12 sm:p-4">
         <h2 class="text-3xl text-slate-500">Oops!! Page Not Available</h2>
-        <img src="../assets/Scarecrow.png" alt="Working on Page" class="w-5/12 mx-auto sm:w-8/12">
+        <img src="../assets/Scarecrow.png" alt="Working on Page" class="w-5/12 mx-auto sm:w-full
+        ">
         <p class="text-lg font-thin text-red-700">The page build is currently in progress. Will be updated soon</p>
     </div>
   </div>
