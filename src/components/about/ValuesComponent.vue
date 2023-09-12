0f438691d3e69e66a12491b5933111b0e31497ca
Mfoniso Fabian <goodlucketim94@gmail.com>
2023-09-12 15:59:24 +0100

    accordion

--- src/components/about/ValuesComponent.vue
+++ src/components/about/ValuesComponent.vue
@@ -2,30 +2,19 @@
     <div>
         <div class="values w-11/12 mx-auto">
             <h2 class="text-center font-bold text-3xl p-8 sm:text-2xl">Our Values</h2>
-            <div class="values-container">
-                <div class="value bg-blue-900 px-4 py-2 rounded-lg flex justify-between text-white align-center my-3">
-                    <p>Passion</p>
-                    <font-awesome-icon icon="fa-solid fa-circle-chevron-down" />
-                </div>
-                <div class="value bg-blue-900 px-4 py-2 rounded-lg flex justify-between text-white align-center my-3">
-                    <p>Excellence</p>
-                    <font-awesome-icon icon="fa-solid fa-circle-chevron-down" />
-                </div>
-                <div class="value bg-blue-900 px-4 py-2 rounded-lg flex justify-between text-white align-center my-3">
-                    <p>Team Work</p>
-                    <font-awesome-icon icon="fa-solid fa-circle-chevron-down" />
-                </div>
-                <div class="value bg-blue-900 px-4 py-2 rounded-lg flex justify-between text-white align-center my-3">
-                    <p>Quality Service</p>
-                    <font-awesome-icon icon="fa-solid fa-circle-chevron-down" />
-                </div>
-                <div class="value bg-blue-900 px-4 py-2 rounded-lg flex justify-between text-white align-center my-3">
-                    <p>Transparency</p>
-                    <font-awesome-icon icon="fa-solid fa-circle-chevron-down" />
-                </div>
-                <div class="value bg-blue-900 px-4 py-2 rounded-lg flex justify-between text-white align-center my-3">
-                    <p>Influence</p>
-                    <font-awesome-icon icon="fa-solid fa-circle-chevron-down" />
+            <div class="values-container" >
+                <div class="accordion" v-for="(accord, index) in accordions" :key="index" @click="toggleAccordion(index)">
+                    <div class="value bg-blue-900 px-4 py-2 rounded-lg flex justify-between text-white align-center my-3">
+                        <p>{{ accord.title }}</p>
+                        <font-awesome-icon icon="fa-solid fa-circle-chevron-down" />
+                    </div>
+                    <transition name="slide-fade">
+                        <div class="accordion-content shadow p-5 h-20" v-show="index === openIndex">
+                            <p>
+                                {{ accord.content }}
+                            </p>
+                        </div>
+                    </transition>
                 </div>
             </div>
         </div>
@@ -35,10 +24,72 @@
   <script>
   export default {
     name: "valuesComponent",
+    data() {
+        return {
+            openIndex: -1,
+           accordions:[
+            {
+                title: 'Passion',
+                content: 'We are passionate about what we do.',
+                open: false
+            },
+            {
+                title: 'Excellence',
+                content: 'We are passionate about what we do.',
+                open: false
+            },
+            {
+                title: 'Quality Service',
+                content: 'We are passionate about what we do.',
+                open: false
+            },
+            {
+                title: 'Team Work',
+                content: 'We are passionate about what we do.',
+                open: false
+            },
+            {
+                title: 'Transparency',
+                content: 'We are passionate about what we do.',
+                open: false
+            },
+            {
+                title: 'Influence',
+                content: 'We are passionate about what we do.',
+                open: false
+            }
+           ] 
+        }
+    },
+    methods: {
+        toggleAccordion(index) {
+           if(this.openIndex === index){
+            this.openIndex = -1
+           }else{
+            this.openIndex = index
+           }
+        }
+    },
   };
   </script>
   
   <style scoped>
-  
+    .accordion-content{
+        overflow: hidden;
+    }
+    .slide-fade-enter-active {
+        transition: all 0.5s ease;
+    }
+    .slide-fade-leave-active {
+        transition: all 0.5s ease;
+    }
+    .slide-fade-enter, .slide-fade-leave-to {
+        transform: translateY(0);
+        opacity:0;
+    }
+    .slide-fade-leave-active, .slide-fade-enter-to {
+        transform: translateY(-100%);
+        opacity: 0;
+    }
   </style>
   
