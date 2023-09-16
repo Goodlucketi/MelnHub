28197f3df7346976febaee6fa08c4b6f26624fdd
Mfoniso Fabian <goodlucketim94@gmail.com>
2023-09-16 16:28:16 +0100

    Update

--- src/components/register/RegistrationForm.vue
+++ src/components/register/RegistrationForm.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="register">
     <div class="regContainer w-11/12 py-10 mx-auto">
-      <div class="form flex gap-5 justify-center align-center">
-        <form class="regForm border-l border-slate-600 rounded-xl p-4 w-5/12">
+      <div class="form flex gap-5 justify-center align-center lg:flex-col">
+        <form class="regForm border-l border-slate-600 rounded-xl p-4 w-5/12 lg:mx-auto lg:w-8/12 sm:w-11/12">
           <h3 class="text-2xl text-center mb-3 p-2">Register</h3>
 
           <div class="p-2 border-b mb-3 relative ">
@@ -46,8 +46,8 @@
             <input type="submit" value="Enroll" class="block w-11/12 mx-auto bg-purple-700 text-white p-2 rounded-lg hover:bg-slate-400 transition-all duration-1000">
           </div> 
         </form>
-        <div class="form-img w-5/12 relative rounded-xl">
-          <h3 class="p-4 text-center text-2xl absolute bottom-32">
+        <div class="form-img w-5/12 relative rounded-xl lg:mx-auto lg:w-8/12 sm:w-11/12">
+          <h3 class="p-4 text-center text-2xl absolute bottom-32 sm:bottom-8">
             We are <span class="text-4xl text-purple-800 text-shadow">Transforming</span> the <span class="text-4xl text-purple-800 text-shadow">Future</span> with <span class="text-4xl text-purple-800 text-shadow">Technology</span>
           </h3>
           <img src="../../assets/images/Rectangle 124 (1).png" alt="Laptop wit mobile phone" class="w-full p-4 rounded-xl  transition-all duration-1000 hover:filter-none">
